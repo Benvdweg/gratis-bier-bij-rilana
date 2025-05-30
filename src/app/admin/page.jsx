61c0ac49bb87e9bd8a357f0c0ac48a765bd34e92
Benvdweg <b.vanderweg@student.avans.nl>
2025-05-30 22:04:47 +0200

    Update page.jsx

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -4,12 +4,8 @@ import LogoutButton from "./components/LogoutButton";
 
 export default function Admin() {
 	return (
-		<div>
+		<div className="text-center pt-36 text-4xl space-y-24">
 			<h1>Admin Panel</h1>
-			<div>
-				<button>Add Beer Stats</button>
-				<button>Manage Users</button>
-			</div>
 			<LogoutButton></LogoutButton>
 		</div>
 	);
