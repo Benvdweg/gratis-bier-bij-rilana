9e06332e7b53bb17b492df7d1352dbb89c6575c5
Benvdweg <b.vanderweg@student.avans.nl>
2025-06-21 02:19:43 +0200

    Move Components folder to root level

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Navbar from "@/app/components/NavBar";
+import Navbar from "@/components/NavBar";
 
 export default function AppWrapper({
 	children,
