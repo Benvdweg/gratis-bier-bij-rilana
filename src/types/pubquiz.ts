fcfad3843ef26ad9e2c14bf03d17860b98b50c96
Benvdweg <b.vanderweg@student.avans.nl>
2025-06-22 23:24:00 +0200

    Add types

---
+++ src/types/pubquiz.ts
@@ -0,0 +1,4 @@
+export interface Pubquiz {
+	id: number;
+	name: string;
+}
---
+++ src/types/pubquizResult.ts
@@ -0,0 +1,6 @@
+export interface PubquizResult {
+	id?: number;
+	user_id: number;
+	pubquiz_id: number;
+	placement: number;
+}
