1272827cd2a67f8089cac71eba5b0cb51025850b
Benvdweg <b.vanderweg@student.avans.nl>
2025-06-22 23:00:52 +0200

    Update page.tsx

--- src/app/pubquiz/page.tsx
+++ src/app/pubquiz/page.tsx
@@ -2,14 +2,16 @@ import { ChampionCard } from "./components/ChampionCard";
 
 export default function PubquizPage() {
 	return (
-		<div className="mt-8 sm:mt-16 md:mt-32 bg-[#fcfbfb] w-full max-w-4xl p-4 sm:p-6 md:p-8 rounded-xl mx-4 sm:mx-auto">
-			<h1 className="text-black font-bold text-2xl sm:text-3xl md:text-4xl text-center mb-4 sm:mb-6">
-				🏆 Hall of Champions
-			</h1>
-			<div className="text-gray-800">
-				<ChampionCard name={"Ben"} edition={["Tsjechië", "AI"]} />
-				<ChampionCard name={"Tijmen"} edition={["Tenzin Editie"]} />
-				<ChampionCard name={"Nick"} edition={["Kempervennen"]} />
+		<div className="px-4 sm:px-0">
+			<div className="mt-8 sm:mt-16 md:mt-32 bg-[#fcfbfb] max-w-4xl mx-auto p-4 sm:p-6 md:p-8 rounded-xl">
+				<h1 className="text-black font-bold text-2xl sm:text-3xl md:text-4xl text-center mb-4 sm:mb-6">
+					🏆 Hall of Champions
+				</h1>
+				<div className="text-gray-800">
+					<ChampionCard name={"Ben"} edition={["Tsjechië", "AI"]} />
+					<ChampionCard name={"Tijmen"} edition={["Tenzin Editie"]} />
+					<ChampionCard name={"Nick"} edition={["Kempervennen"]} />
+				</div>
 			</div>
 		</div>
 	);
