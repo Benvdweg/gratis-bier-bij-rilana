2bac55b5e85fefd78174c88c5759a69ae9cc7f6a
Benvdweg <b.vanderweg@student.avans.nl>
2025-06-22 21:00:45 +0200

    Create not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+	return (
+		<div className="text-4xl text-center min-h-screen pt-48">
+			<div>
+				<h1>404 - Page Not Found</h1>
+				<p>The page you’re looking for does not exist.</p>
+			</div>
+		</div>
+	);
+}
