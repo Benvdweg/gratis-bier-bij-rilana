5d2dbfb7de9e4ab98f32f04d2a26a9d80fe6a04c
Benvdweg <b.vanderweg@student.avans.nl>
2025-06-22 22:38:26 +0200

    Add hall of champions

--- src/app/pubquiz/page.tsx
+++ src/app/pubquiz/page.tsx
@@ -1,7 +1,33 @@
 export default function PubquizPage() {
 	return (
-		<p className="text-center mt-32 text-4xl font-bold">
-			De Winnaar van de Pubquiz is...
-		</p>
+		<div className="mt-32 bg-[#fcfbfb] w-200 mx-auto p-8 rounded-xl">
+			<h1 className="text-black  font-bold text-4xl text-center">
+				🏆 Hall of Champions
+			</h1>
+			<div className="text-gray-800 text-2xl mb-6">
+				<div className="flex flex-row bg-gray-200 shadow-xl mt-8 rounded-xl py-3">
+					<div className="my-auto text-3xl ml-3">🥇</div>
+					<div className="ml-3">
+						<p className="font-bold">Ben</p>
+						<p>Tsjechië Editie</p>
+						<p>AI Editie</p>
+					</div>
+				</div>
+				<div className="flex flex-row bg-gray-200 shadow-xl mt-8 rounded-xl py-3">
+					<div className="my-auto text-3xl ml-3">🥇</div>
+					<div className="ml-3">
+						<p className="font-bold">Tijmen</p>
+						<p>Tenzin Editie</p>
+					</div>
+				</div>
+				<div className="flex flex-row bg-gray-200 shadow-xl mt-8 rounded-xl py-3">
+					<div className="my-auto text-3xl ml-3">🥇</div>
+					<div className="ml-3">
+						<p className="font-bold">Nick</p>
+						<p>Kempervennen Editie</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
